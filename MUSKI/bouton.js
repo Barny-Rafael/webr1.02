401e1ebfbca3ee94ea87bcd1eba95fdd53a2bacf
Barny-Rafael <rafael.barny@etu.univ-amu.fr>
2025-12-17 16:08:16 +0100

    changement de thème et fusion des thèmes et des js

--- MUSKI/bouton.js
+++
@@ -1,9 +0,0 @@
-function changerStyle() {
-  const theme = document.getElementById("theme");
-
-  if (theme.getAttribute("href") === "styles/style_noir.css") {
-    theme.setAttribute("href", "styles/style.css");
-  } else {
-    theme.setAttribute("href", "styles/style_noir.css");
-  }
-}
--- MUSKI/bouton_accueil.js
+++
@@ -1,9 +0,0 @@
-function changerStyle() {
-    const theme = document.getElementById("theme");
-
-    if (theme.getAttribute("href") === "styles/accueil_blanc.css") {
-        theme.setAttribute("href", "styles/accueil.css");
-    } else {
-        theme.setAttribute("href", "styles/accueil_blanc.css");
-    }
-}
